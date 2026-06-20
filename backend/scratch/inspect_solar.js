ee683df436f76cfa3d86fafcadc20514b8780b1e
tuynover <cobatuoc@gmail.com>
2026-06-20 18:53:05 +0700

    Delete backend/scratch directory

--- backend/scratch/inspect_solar.js
+++
@@ -1,9 +0,0 @@
-const { Solar, Lunar } = require('lunar-javascript');
-
-console.log('Solar methods:');
-console.log(Object.getOwnPropertyNames(Solar));
-console.log(Object.getOwnPropertyNames(Solar.prototype));
-
-console.log('\nLunar methods:');
-console.log(Object.getOwnPropertyNames(Lunar));
-console.log(Object.getOwnPropertyNames(Lunar.prototype));
--- backend/scratch/print_lunar_methods.js
+++
@@ -1,9 +0,0 @@
-const { Solar } = require('lunar-javascript');
-const s = Solar.fromYmdHms(2026, 5, 24, 12, 0, 0);
-const l = s.getLunar();
-console.log('Lunar keys:');
-for (let p in l) {
-    if (typeof l[p] === 'function') {
-        console.log('Method:', p);
-    }
-}
